70c3958ad5706ffa6c016a96495097b073dfcb6f
edgarx842 <edgar.mamani@ucb.edu.bo>
2025-02-25 12:19:04 -0400

    añadir

---
+++ practica1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Mostrar el directorio actual
+pwd
+
+# Acceder a la carpeta /tmp y listar su contenido
+cd /tmp
+ls
+
+# Volver al directorio personal
+cd ~
+
+# Crear la carpeta practica_shell (si no existe)
+mkdir -p practica_shell
+
+# Acceder a la carpeta practica_shell
+cd practica_shell
+
+# Mostrar el directorio actual
+pwd
