5c65544159e46d8bcb8adcac431018017cbf43ce
edgarx842 <edgar.mamani@ucb.edu.bo>
2025-03-02 18:21:02 -0400

    tarea

---
+++ practica2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir -p ~/practica_shell
+touch archivo1.txt
+echo "Hola, este es un archivo de prueba" > archivo1.txt
+cp archivo1.txt archivo_copia.txt
+mkdir -p ~/practica_shell/backup
+mv archivo_copia.txt ~/practica_shell/backup/
+rm archivo1.txt
+ls ~/practica_shell/backup
